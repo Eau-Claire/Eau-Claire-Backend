efc76e8461ab5971c66f2837e47bcfccd2e6e20e
Lu.Bu <170906249+bunpmc@users.noreply.github.com>
2025-10-30 10:23:27 +0700

    test: render, added client.SendCompleted in sending email to log error

--- Services/OtpService.cs
+++ Services/OtpService.cs
@@ -109,6 +109,10 @@ namespace FishFarm.Services
 
                         await client.SendMailAsync(mailMessage);
 
+                        client.SendCompleted += (s, e) => {
+                            Console.WriteLine($"SMTP error: {e.Error?.Message}");
+                        };
+
                         return new ServiceResult
                         {
                             IsSuccess = true,
