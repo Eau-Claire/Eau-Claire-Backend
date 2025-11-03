4e9a60081f712d2f86c7466854e81b38b01f9cb1
Lu.Bu <170906249+bunpmc@users.noreply.github.com>
2025-11-03 09:55:33 +0700

    test: minio on render

--- FishFarmAPI_v2/Controllers/MinioController.cs
+++ FishFarmAPI_v2/Controllers/MinioController.cs
@@ -20,7 +20,7 @@ namespace FishFarmAPI_v2.Controllers
         {
             try
             {
-                var minioUrl = $"http://10.24.0.175:9000/{path}";
+                var minioUrl = $"http://100.98.59.51:9000/{path}";
 
                 // Chỉ đọc header, tai ve dan dan
                 using var response = await _httpClient.GetAsync(minioUrl, HttpCompletionOption.ResponseHeadersRead);
