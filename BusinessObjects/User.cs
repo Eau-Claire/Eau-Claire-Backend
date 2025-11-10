e82a51488195346f34529c0b41af013c432e6c0f
Minh Châu <phamhoangminhchau1973@gmail.com>
2025-11-10 16:10:29 +0700

    test: supabase update query

--- BusinessObjects/User.cs
+++ BusinessObjects/User.cs
@@ -6,17 +6,11 @@ namespace FishFarm.BusinessObjects;
 
 public partial class User
 {
-    [Column("user_id")]
     public int UserId { get; set; }
-    [Column("username")]
     public string Username { get; set; } = null!;
-    [Column("password_hash")]
     public string PasswordHash { get; set; } = null!;
-    [Column("email")]
     public string? Email { get; set; }
-    [Column("phone")]
     public string? Phone { get; set; }
-    [Column("role")]
     public string? Role { get; set; }
 
     public virtual ICollection<Notification> Notifications { get; set; } = new List<Notification>();
